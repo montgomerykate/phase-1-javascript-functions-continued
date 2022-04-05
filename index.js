0a3111a86690bcdebe01c40c06f7e257149dfb67
Kate Montgomery <katemontgomery431@gmail.com>
2022-04-05 11:07:25 -0400

    commit tree 74b709d07647ca4ca43b33bd26bb7f0dbfc01646

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+  }
+saturdayFun("wash my dog")
+function mondayWork(activity = "go to the office") {
+    return(`This Monday, I will ${activity}.`)
+}
+mondayWork("work from home")
+
+function wrapAdjective() {
+    const part1 = "You are";
+    return function () {
+      const part2 = "a dedicated programmer";
+      return function () {
+        console.log(`${part1} ${part2}!`);
+      };
+    };
+  }
+  wrapAdjective("*")("a dedicated programmer");
